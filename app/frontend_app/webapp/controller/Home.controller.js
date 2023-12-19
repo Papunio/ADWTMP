62add505d450d8cf31c6a03c266febd6fe757f11
Patryk Mazur <paputogej@gmail.com>
2023-12-19 17:12:23 +0000

    Add Routing & SmartTable & Linter

--- app/frontend_app/webapp/controller/Home.controller.js
+++ app/frontend_app/webapp/controller/Home.controller.js
@@ -1,15 +1,27 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller) {
+    "use strict";
 
-        return Controller.extend("frontendapp.controller.Home", {
-            onInit: function () {
-
-            }
-        });
+    return Controller.extend("frontendapp.controller.Home", {
+      onInit: function () {
+        const oModel = new sap.ui.model.odata.v2.ODataModel("/v2/football/");
+      },
+      onPressAddMatch: function () {
+        const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+        oRouter.navTo("AddMatch");
+      },
+      onPressTeams: function () {
+        const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+        oRouter.navTo("TeamsList");
+      },
+      onPressPlayers: function () {
+        const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+        oRouter.navTo("PlayersList");
+      },
     });
+  }
+);
--- db/schema.cds
+++ db/schema.cds
@@ -20,18 +20,18 @@ entity Players : cuid {
 @title: 'Entity that represents football match'
 entity Matches : cuid {
     @title: 'Team1 name'
-    t1Name    : String(100);
+    t1Name    : String(80);
 
     @title: 'Players in Team1'
     t1Players : Composition of many {
                     key player : Association to Players;
                 };
 
-    @title: 'Score of the game'
+    @title: 'Score'
     score     : String(5);
 
     @title: 'Team2 name'
-    t2Name    : String(100);
+    t2Name    : String(80);
 
     @title: 'Players in Team2'
     t2Players : Composition of many {
