d5cdf432846c1a71693bda200ad634abd0f97235
Patryk Mazur <paputogej@gmail.com>
2023-12-24 15:20:35 +0000

    Add team models to MatchDetails controller

--- app/frontend_app/webapp/controller/MatchDetails.controller.js
+++ app/frontend_app/webapp/controller/MatchDetails.controller.js
@@ -10,8 +10,11 @@ sap.ui.define(
     return BaseController.extend("frontendapp.controller.MatchDetails", {
       onInit: function () {
         const oComponent = this.getOwnerComponent();
-        const oMatchDetails = new JSONModel();
-        oComponent.setModel(oMatchDetails, "matchDetailsModel");
+        const oTeam1Details = new JSONModel();
+        const oTeam2Details = new JSONModel();
+
+        oComponent.setModel(oTeam1Details, "team1DetailsModel");
+        oComponent.setModel(oTeam2Details, "team2DetailsModel");
         const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
         oRouter
           .getRoute("MatchDetails")
@@ -24,27 +27,74 @@ sap.ui.define(
 
         const oModel = new sap.ui.model.odata.v2.ODataModel("/v2/football");
 
-        const oMatchDetails =
-          this.getOwnerComponent().getModel("matchDetailsModel");
-        oModel.read(`/Matches/${oEvent.getParameter("arguments").MatchID}`, {
-          urlParameters: {
-            $expand: "teams",
-          },
-          success: (oData) => {
-            const aTeams = oData.teams.results;
-            aTeams.forEach((oTeam) => {
-              // console.log(oTeam);
-              oModel.read(`/Teams/${oTeam.up__ID}`, {
+        const oTeam1Details =
+          this.getOwnerComponent().getModel("team1DetailsModel");
+        const oTeam2Details =
+          this.getOwnerComponent().getModel("team2DetailsModel");
+
+        const oPromise = new Promise((resolve) => {
+          oModel.read(`/Matches/${oEvent.getParameter("arguments").MatchID}`, {
+            urlParameters: {
+              $expand: "teams",
+            },
+            success: (oData) => {
+              const aTeams = oData.teams.results;
+              // TUTAJ ŁADNIEJ (DRY) PĘTLA CZY COS XD
+              oModel.read(`/Teams/${aTeams[0].up__ID}`, {
+                urlParameters: {
+                  $expand: "players",
+                },
                 success: (oTeamData) => {
-                  console.log(oTeamData);
+                  const aTeam1Data = {};
+                  aTeam1Data.name = oTeamData.name;
+                  const aPlayers = [];
+
+                  oTeamData.players.results.forEach((oPlayer) => {
+                    oModel.read(`/Players/${oPlayer.player_ID}`, {
+                      success: (oPlayerData) => {
+                        aPlayers.push(oPlayerData);
+                      },
+                    });
+                  });
+                  aTeam1Data.players = aPlayers;
+                  oTeam1Details.setData(aTeam1Data);
                 },
               });
-            });
-          },
-          error: (oErr) => {
-            MessageBox.error("{i18n>Something went wrong}");
-            console.error(oErr.message);
-          },
+
+              oModel.read(`/Teams/${aTeams[1].up__ID}`, {
+                urlParameters: {
+                  $expand: "players",
+                },
+                success: (oTeamData) => {
+                  const aTeam2Data = {};
+                  aTeam2Data.name = oTeamData.name;
+                  const aPlayers = [];
+
+                  oTeamData.players.results.forEach((oPlayer) => {
+                    oModel.read(`/Players/${oPlayer.player_ID}`, {
+                      success: (oPlayerData) => {
+                        aPlayers.push(oPlayerData);
+                      },
+                    });
+                  });
+                  aTeam2Data.players = aPlayers;
+                  oTeam2Details.setData(aTeam2Data);
+                },
+              });
+            },
+            error: (oErr) => {
+              MessageBox.error("{i18n>Something went wrong}");
+              console.error(oErr.message);
+            },
+          });
+          resolve();
+        });
+
+        oPromise.then(() => {
+          console.log(oTeam1Details);
+          console.log(oTeam2Details);
+          // oMatchesModel.setData(aTeams);
+          // oView.setModel(oMatchesModel, "MatchesModel");
         });
       },
     });
